e1af139b7faefc49b643551d8090e6e85560715e
Yury Lebedev <y.d.lebedev@gmail.com>
2019-12-28 13:21:22 -0500

    Added loading screen to login/register

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,19 +7,24 @@ const jwt = require('jsonwebtoken');
 const secret = 'hj2bas2361d55hKJhd9AHd9a.asd0121';
 
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
-    console.log('here');
-    const user = await db.user.findOne({ where: { firstName: 'Yury' } });
-    console.log(user.dataValues);
-    res.status(200).send({ message: 'ok' });
-});
+// router.get('/', async (req, res, next) => {
+//     // console.log('here');
+//     // const user = await db.user.findOne({ where: { firstName: 'Yury' } });
+//     // console.log(user.dataValues);
+//     // res.status(200).send({ message: 'ok' });
+// });
 
 // Register new user
 router.post('/register', async (req, res, next) => {
     const { firstName, lastName, email, password: pass } = req.body;
     const password = await bcrypt.hash(pass, 10);
-    const ensureUnique = await db.user.findOne({ where: { email } });
-    if (ensureUnique) return res.status(400).send({ message: 'User already registered.' });
+    const ensureUnique = await db.user.findOne({
+        where: { email },
+    });
+    if (ensureUnique)
+        return res.status(400).send({
+            message: 'User already registered',
+        });
     const user = await db.user.create({
         firstName,
         lastName,
@@ -28,17 +33,24 @@ router.post('/register', async (req, res, next) => {
     });
 
     if (user) return res.status(200).send({ message: 'ok' });
-    else return res.status(500).send({ message: 'There was an error registering user.' });
+    else
+        return res.status(500).send({
+            message: 'There was an error registering user',
+        });
 });
 
 // Login existing user
 router.post('/login', async (req, res, next) => {
     console.log(req.body);
     const { email, password } = req.body;
-    const user = await db.user.findOne({ where: { email } });
+    const user = await db.user.findOne({
+        where: { email },
+    });
     // No such user
     if (!user)
-        return res.status(400).send({ message: 'Please ensure email and password are correct' });
+        return res.status(400).send({
+            message: 'Please ensure email and password are correct',
+        });
     // Verify password
     if (await bcrypt.compare(password, user.dataValues.password)) {
         // create JWT
@@ -46,7 +58,10 @@ router.post('/login', async (req, res, next) => {
         // Set cookie
         res.cookie('jwt', token);
         return res.status(200).send({ message: 'ok' });
-    } else return res.status(400).send({ message: 'Please ensure email and password are correct' });
+    } else
+        return res.status(400).send({
+            message: 'Please ensure email and password are correct',
+        });
 });
 
 router.post('/logout', async (req, res, next) => {
